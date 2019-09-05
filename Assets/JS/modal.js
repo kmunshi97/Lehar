3531d64408a873daca3fd4a9059ae1c2c6f1f690
kmunshi97 <kmunshi.97@gmail.com>
2019-09-05 15:10:18 +0530

    Added Modal!

---
+++ Assets/JS/modal.js
@@ -0,0 +1,9 @@
+$('#yes-modal')
+    .modal('attach events', '#yes-button', 'show')
+    .modal('setting', 'closable', false)
+    ;
+
+$('#no-modal')
+    .modal('attach events', '#no-button', 'show')
+    .modal('setting', 'closable', false)
+    ;
