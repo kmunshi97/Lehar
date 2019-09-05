e19a94f0e3cf2ba4d3b23037bc1f9ba30539700f
kmunshi97 <kmunshi.97@gmail.com>
2019-09-05 20:30:44 +0530

    Testing v.1.0.0

--- Assets/JS/modal.js
+++ Assets/JS/modal.js
@@ -1,9 +1,15 @@
 $('#yes-modal')
     .modal('attach events', '#yes-button', 'show')
+    .modal({ centered: false })
     .modal('setting', 'closable', false)
     ;
 
 $('#no-modal')
     .modal('attach events', '#no-button', 'show')
+    .modal({ centered: false })
     .modal('setting', 'closable', false)
+    ;
+
+$('.click-here')
+    .transition('shake')
     ;
